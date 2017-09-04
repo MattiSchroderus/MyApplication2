460a5836aa89a4b8932eced381319e8a44d0d4a9
Matti Schroderus <mattischroderus37@gmail.com>
2017-09-04 10:17:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/matti/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+
+package com.example.matti.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void buttonClicked(View view) {
+// find radiogroup
+        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
+        int id = rg.getCheckedRadioButtonId();
+// find button
+        RadioButton rb = (RadioButton) findViewById(id);
+// get radiobutton text
+        String text = (String) rb.getText();
+// toast message to screen
+        Toast.makeText(getApplicationContext(), text,
+                Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+}
